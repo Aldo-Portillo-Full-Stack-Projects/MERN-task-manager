33cdb1f50d26b0746c852d3bed3655bf69b4e01e
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-08 16:17:03 -0600

    Create state for form data

--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,11 +1,42 @@
 import React from 'react'
+import { toast } from 'react-toastify'
 import TaskForm from './TaskForm'
+import axios from 'axios'
 
 export default function TaskList() {
+
+    const [formData, setFormData] = React.useState({
+        name: "",
+        completed: false
+    })
+
+    const {name} = formData
+
+    const handleInputChange = (e) => {
+
+        const {name, value} = e.target;
+
+        setFormData({...formData, [name]: value})
+    }
+
+    const createTask = async (e) => {
+        e.preventDefault();
+        
+        if(name === ""){
+            return toast.error("Input field cannot be empty")
+        }
+
+        try{
+            await axios.post()
+        } catch (err) {
+
+        }
+    }
+
   return (
     <div>
         <h2>Task Manager</h2>
-        <TaskForm />
+        <TaskForm name={name} handleInputChange={handleInputChange} createTask={createTask}/>
     </div>
   )
 }
