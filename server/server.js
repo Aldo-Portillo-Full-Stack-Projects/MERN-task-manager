e8580809033f5d30ec447b7a74779a98e0fc64a8
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-08 13:27:30 -0600

    Connect to database

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
-const { application } = require("express")
+const dotenv = require("dotenv").config() //Essential: It makes sure that server.js can use the URI in the .env file
 const express = require("express")
+const connectDB = require('./config/connectDB')
 
 const app = express()
+
+connectDB() //Connects to database from the file inside config
 
 const PORT = process.env.PORT || 5000 //Allows heroku to create port and if none place port 5000
 app.listen(PORT, () => {
