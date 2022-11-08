52c30d429a61f61949c003d17e55459ac8d1491a
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-08 13:54:13 -0600

    Accept data passed into post using middleware

--- server/server.js
+++ server/server.js
@@ -16,8 +16,24 @@ mongoose
     })
     .catch((err) => console.log(err))
 
+//Middleware: Function that can be slotted inside routes that have access to req objects and response objects and next function
+
+app.use(express.json()) //Gives us data passed into body
+
+app.use(express.urlencoded({extended: false})) //Gives data passed into form
 
 //Routes
 app.get("/", (req, res) => {
     res.send("<h1>Home Page</h1>")
+})
+
+app.get("/tasks", (req, res) => {
+    res.send("<h1>Tasks Page</h1>")
+})
+
+//Create a task
+
+app.post("/api/tasks", async (req, res) => {
+    console.log(req.body)
+    res.send("Task Created")
 })
