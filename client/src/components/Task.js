62a6104a048fda795e47da1235105dae538e0a5b
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-08 16:07:37 -0600

    Render Icons

--- client/src/components/Task.js
+++ client/src/components/Task.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import {FaCheckDouble, FaEdit, FaRegTrashAlt} from 'react-icons/fa'
 
 export default function Task() {
   return (
-    <div>Task</div>
+    <div className='task'>
+        <p>
+            <b>1. </b>
+            Task 1
+        </p>
+        <div className='task-icons'>
+            <FaCheckDouble color='green' />
+            <FaEdit color='purple' />
+            <FaRegTrashAlt color='red' />
+        </div>
+    </div>
   )
 }
