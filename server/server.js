257f2c4ceb8f7dad1258e967521a288484b7c3fd
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-08 12:58:25 -0600

    Create express server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const { application } = require("express")
 const express = require("express")
 
 const app = express()
@@ -5,4 +6,8 @@ const app = express()
 const PORT = process.env.PORT || 5000 //Allows heroku to create port and if none place port 5000
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
+})
+//Routes
+app.get("/", (req, res) => {
+    res.send("<h1>Home Page</h1>")
 })
