4dee8e86e50c99f2c6476090e5aebeea700b982f
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-08 16:55:09 -0600

    Pass props down to Task.js

--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -90,7 +90,7 @@ export default function TaskList() {
                 <>
                 {tasks.map((task, index) => {
                     return(
-                        <Task />
+                        <Task key={task._id} task={task} index={index}/>
                     )
                 })}
                 </>
