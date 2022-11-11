7ef904a2ae4a4dcada25549cda3c551c652d5dbe
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-11-11 00:04:25 -0600

    Make onSubmit dynamic

--- client/src/components/TaskForm.js
+++ client/src/components/TaskForm.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function TaskForm({createTask, name, handleInputChange, isEditing}) {
+export default function TaskForm({createTask, name, handleInputChange, isEditing, updateTask}) {
   return (
-    <form className='task-form' onSubmit={createTask}>
+    <form className='task-form' onSubmit={isEditing? updateTask :createTask}>
         <input type="task" placeholder='add a task' name="name" value={name} onChange={handleInputChange} />
         <button type="submit">{isEditing? "Edit" : "Add"}</button>
     </form>
